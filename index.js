9c6a86d558c25ebcea720771ae0d98dd62dfd29b
Hilful <hilful25@gmail.com>
2022-02-13 02:40:16 +0600

    basic node practice using hyper cli and node

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes = require('superheroes');
+var mySuperHeroName = superheroes.random();
+console.log("Name of the hero is : " + mySuperHeroName);
+const supervillains = require('supervillains');
+var mySuperVillain = supervillains.random();
+console.log("Name of the villan is : " +mySuperVillain);
